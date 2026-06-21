a9a0b6b7ebb3533a3d53d1b27237a5aed2115791
Javier <J_sedanomar@uncg.edu>
2026-06-21 15:09:50 -0400

    some other initial changes

---
+++ valomegle/src/main/java/com/sedanodev/valomegle/controllers/UserController.java
@@ -0,0 +1,5 @@
+package com.sedanodev.valomegle.controllers;
+
+public class UserController {
+
+}
---
+++ valomegle/src/main/java/com/sedanodev/valomegle/models/User.java
@@ -0,0 +1,27 @@
+package com.sedanodev.valomegle.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
