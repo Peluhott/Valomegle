3590e23b03d4fc04f14c4a08e828cf42c57fc0db
Javier <J_sedanomar@uncg.edu>
2026-06-21 12:14:08 -0400

    initialize spring java repo

---
+++ valomegle/src/main/java/com/sedanodev/valomegle/ValomegleApplication.java
@@ -0,0 +1,13 @@
+package com.sedanodev.valomegle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValomegleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValomegleApplication.class, args);
+	}
+
+}
---
+++ valomegle/src/test/java/com/sedanodev/valomegle/ValomegleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sedanodev.valomegle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValomegleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
